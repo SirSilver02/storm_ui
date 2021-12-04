393bea8ab777da93fdc860c70298b2c692a057a8
Sir_Silver <mjcastro50@gmail.com>
2021-12-04 09:01:20 -0700

    Added comments, removed test code.

--- modules/colored_text.lua
+++ modules/colored_text.lua
@@ -1,7 +1,6 @@
 --word_colors is a table with words as keys and colors as values
 
-local iterations = 0
-
+--returns a colored_text table that is usable with love.graphics.newText()
 local function colored_text(text, word_colors, default_color)
     default_color = default_color or {1, 1, 1}
     local lower_text = text:lower()
@@ -9,17 +8,17 @@ local function colored_text(text, word_colors, default_color)
     local colored_text = {original_text = text}
     local matches = {}
 
+    --keeps track of the start and end substring pos of each word match
     for word, color in pairs(word_colors) do
-        iterations = iterations + 1
         local start, stop = lower_text:find(word)
 
         while start do
-            iterations = iterations + 1
             table.insert(matches, {start = start, stop = stop, color = color})
             start, stop = lower_text:find(word, stop)
         end
     end
 
+    --sorts by which came first, necessary step.
     table.sort(matches, function(a, b)
         return a.start < b.start
     end)
@@ -27,25 +26,30 @@ local function colored_text(text, word_colors, default_color)
     local string_i = 1
     local matches_i = 1
 
-    while string_i < #text + 1 do
+    --while string_i < #text + 1 do
+    while true do
         local match = matches[matches_i]
 
         if match then
             if match.start == string_i then
+                --have a match and currently dealing with it.
                 table.insert(colored_text, match.color)
                 table.insert(colored_text, text:sub(match.start, match.stop))
 
                 string_i = match.stop
                 matches_i = matches_i + 1
             else
+                --if we have a match but we have non matches that we have to deal with.
                 table.insert(colored_text, default_color)
                 table.insert(colored_text, text:sub(string_i, match.start - 1))
 
                 string_i = match.start - 1
             end
 
+            --start the next substring 1 letter over.
             string_i = string_i + 1
         else
+            --finishing up the end of the string that is not a match.
             table.insert(colored_text, default_color)
             table.insert(colored_text, text:sub(string_i, #text))
 
@@ -53,7 +57,6 @@ local function colored_text(text, word_colors, default_color)
         end
     end
 
-    iterations = 0
     return colored_text
 end
 
